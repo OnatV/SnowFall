446157998a00ba1417f22e3dcc77873ade23684d
Jackson Stanhope <jstanhope@student.ethz.ch>
2023-12-01 17:38:25 +0100

    fixed test

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -5,13 +5,14 @@ from numpy.random import random
 # install: pip install pytest
 # to execute: python -m pytest tests\test_grid.py
 
-particle_r = 0.02
+particle_r = 0.01
 h = 4.0 * particle_r
 
 @ti.kernel
 def grid_contains(val:int, grid_idx:int, grid:ti.template()) -> bool:
     res = False
     ti.loop_config(serialize=True)
+    # print("grid", grid[grid_idx])
     for i in range(grid[grid_idx].length()):
         if grid[grid_idx, i] == val:
             res = True
@@ -22,14 +23,18 @@ def test_one_particle_position(num=10):
     ti.init(arch=ti.cpu, debug=True)
     origin = ti.Vector([0.0]*3)
     end_point = ti.Vector([1.0]*3)
-    fg = FluidGrid(origin, end_point, 1.5 * h)
+    fg = FluidGrid(origin, end_point, h) # grid spacing is 0.12
     positions = ti.Vector.field(3, float, shape=num)
     positions.from_numpy(random((num, 3)))
     fg.update_grid(positions)
         
     @ti.kernel
     def flat_idx(pos:ti.types.vector(3, float)) -> int:
-        return fg.grid_to_array_index(pos[0],pos[1],pos[2])
+        (x,y,z) = fg.to_grid_idx(pos)
+        return fg.grid_to_array_index(x, y, z)
+    
     for i in range(num):
+        print("position", positions[i])
+        print("idx", flat_idx(positions[i]))
         assert grid_contains(i, flat_idx(positions[i]), fg.grid)
 
